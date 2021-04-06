db4f6699065d0cd7c6e84ed8124879b4766cddb9
Nikita-Babich <81920957+Nikita-Babich@users.noreply.github.com>
2021-04-06 11:40:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-for(i = 1; i < 100; i++) {
- if (i) {document.write(result + "<br>");}
+var out1 = document.getElementById("output1");
+for(i = 1; i < 10; i++) {
+ if (i) {out1.write(result + "<br>");}
 }
